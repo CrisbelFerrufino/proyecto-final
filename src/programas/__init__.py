bddfdfa2d42989169b90690e92ce8aa886b3d3aa
CrisbelFerrufino <202505846@est.umss.edu>
2025-12-18 00:13:11 -0400

    Agrega tests para coeficientes A y B

---
+++ src/programas/__init__.py
@@ -0,0 +1,3 @@
+from .coeficiente_A import obtener_pendiente_A
+from .coeficiente_B import obtener_pendiente
+from .ajuste_mmc import ajuste_mmc
--- tests/test_coeficiente_A.py
+++ tests/test_coeficiente_A.py
@@ -1,4 +1,4 @@
-from src.coeficiente_A import obtener_intercepto
+from programas.coeficiente_A import obtener_intercepto
 
 def test_coeficiente_A():
     assert obtener_intercepto(4.5) == 4.5
